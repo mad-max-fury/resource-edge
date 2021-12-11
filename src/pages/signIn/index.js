43fc4bf97ccf74abe662c49b4ae632e2de3d6458
mad-max-fury <chrisnonso8@gmail.com>
2021-12-11 19:39:05 +0000

    completed logIn ui
    finished the logged screen ui with login progress state intialized

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import { useState } from 'react'
 import "./signIn.css"
 const SignIn = () => {
+  const [IsLoggedIn, setIsLoggedIn] = useState(true)
   return (
-    <div className='signIn'>
+    <main className='signIn'>
+      <img src='images/authLogo.svg' />
+      <form>
+        <header>
+          <img src="images/Alogo.svg" />
+        </header>
+        <main>
+          <div>
+            <h3>Log In</h3>
+            <p>Access your resource edge account</p>
+          </div>
+          <div>
+            {
+              IsLoggedIn ? null
+                : <>
+                  <div>
+                    <h3>Ositadinma Nwangwu</h3>
+                    <p>o.nwangwu@genesystechhub.com</p>
+                  </div>
+                  <img src='images/edit-filled.svg'></img>
+                </>
+            }
+          </div>
+          <div>
+            {
+              IsLoggedIn ? <>
+                <h3>Email Address</h3>
+                <div>
+                  <input type="email" placeholder='Enter email' />
+                  <div className='empty-to-fill'></div>
+                </div>
+                <span>✔</span>
+              </>
+                : <>
 
-    </div>
+                  <h3>Password</h3>
+                  <div>
+                    <input type="password" placeholder='Enter password' />
+                    <div className='empty-to-fill' style={{ background: "#0052CC" }}></div>
+                  </div>
+                  <img src='images/watch.svg' />
+                </>
+            }
+          </div>
+          <div>
+            <div className='empty'></div>
+            <a href='#'>Forget password?</a>
+          </div>
+
+        </main>
+
+      </form>
+    </main>
   )
 }
 
